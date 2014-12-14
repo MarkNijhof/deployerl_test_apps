0d635b9fb5ba9aa5753a8e51d9a17feadc3ed2a9
Mark Nijhof <mark.nijhof@cre8ivethought.com>
2014-12-14 13:15:57 +0100

    used incorrect release version

--- test_app_2/start.sh
+++ test_app_2/start.sh
@@ -2,8 +2,8 @@
 
 IP=`hostname -I | cut -f1 -d' '`
 
-rm ./_rel/test_app_2/releases/1/vm.args
-echo "-setcookie s0m3aw3s0m3c00kie document.farm" >> ./_rel/test_app_2/releases/1/vm.args
-echo "-name `hostname`@$IP" >> ./_rel/test_app_2/releases/1/vm.args
+rm ./_rel/test_app_2/releases/0.0.1/vm.args
+echo "-setcookie s0m3aw3s0m3c00kie document.farm" >> ./_rel/test_app_2/releases/0.0.1/vm.args
+echo "-name `hostname`@$IP" >> ./_rel/test_app_2/releases/0.0.1/vm.args
 
 ./_rel/test_app_2/bin/test_app_2 start
