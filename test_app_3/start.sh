50d9c55d69f808bbe416d98bae300a80f6b9d20b
Mark Nijhof <mark.nijhof@cre8ivethought.com>
2014-12-14 13:26:53 +0100

    created v0.0.3

--- test_app_3/start.sh
+++ test_app_3/start.sh
@@ -2,8 +2,8 @@
 
 IP=`hostname -I | cut -f1 -d' '`
 
-rm ./_rel/test_app_3/releases/0.0.2/vm.args
-echo "-setcookie s0m3aw3s0m3c00kie document.farm" >> ./_rel/test_app_3/releases/0.0.2/vm.args
-echo "-name `hostname`@$IP" >> ./_rel/test_app_3/releases/0.0.2/vm.args
+rm ./_rel/test_app_3/releases/0.0.3/vm.args
+echo "-setcookie s0m3aw3s0m3c00kie document.farm" >> ./_rel/test_app_3/releases/0.0.3/vm.args
+echo "-name `hostname`@$IP" >> ./_rel/test_app_3/releases/0.0.3/vm.args
 
 ./_rel/test_app_3/bin/test_app_3 start
